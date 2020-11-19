6f84a6984a41e4d3212a8845c6111a06d6eb583c
ipung123 <syaifurrohman978@gmail.com>
2020-11-19 11:29:26 +0700

    penambahan button navigation bar

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:food_market/shared/shared.dart';
+import 'package:food_market/ui/widgets/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -11,3 +13,4 @@ part 'general_page.dart';
 part 'sign_in_page.dart';
 part 'sign_up_page.dart';
 part 'address_page.dart';
+part 'main_page.dart';
