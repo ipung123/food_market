81e67cf1c469e01a88136cdefabbe06712b89b57
ipung123 <syaifurrohman978@gmail.com>
2021-02-11 21:48:42 +0700

    penambahan food card

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -47,7 +47,7 @@ class _FoodPageState extends State<FoodPage> {
               ),
             ),
             //// LIST OF FOOD
-            FoodCard()
+            FoodCard(mockFood)
             //// LIST OF FOOD (TABS)
           ],
         )
