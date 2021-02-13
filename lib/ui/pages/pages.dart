32848133749dcd5034b60f3b3a61c0bd11a4aeb6
ipung123 <syaifurrohman978@gmail.com>
2021-02-13 11:50:18 +0700

    food detail page part 2

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -10,7 +10,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
-
+import 'package:intl/intl.dart';
 
 part 'general_page.dart';
 part 'sign_in_page.dart';
